64dcd150fdf3490fdef8bd5fb780cffe564a940e
Dan Fox <fox.j.dan@gmail.com>
2019-08-12 16:15:55 -0700

    Finished most of the advanced content

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,69 +1,95 @@
-var currentPlayer = 1;
-var board = [[0,0,0],[0,0,0],[0,0,0]];
-var occupied = 0;
+var gameState = {
+  occupied: 0,
+  currentPlayer: 1,
+  board: [[0,0,0],[0,0,0],[0,0,0]],
+  p1Name: "Player 1",
+  p1Win: 0,
+  p2Name: "Player 2",
+  p2Win: 0,
+  lastWinner: 1
+}
 
 var clearBoard = function() {
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
-      board[i][j] = 0;
+      gameState.board[i][j] = 0;
     }
   }
   var cells = document.getElementsByClassName("cell");
   for (var i = 0; i < cells.length; i++) {
     cells[i].innerHTML = "";
   }
-  document.getElementById("status").innerHTML = `Player ${currentPlayer}'s turn`;
-  occupied = 0;
+  gameState.occupied = 0;
+  gameState.currentPlayer = gameState.lastWinner;
+  document.getElementById("status").innerHTML = `Player ${gameState.currentPlayer}'s turn`;
 }
 
 var checkWin = function (id, player) {
   id = id.split('');
-  board[id[0]][id[1]] = player;
-  occupied++;
+  gameState.board[id[0]][id[1]] = player;
+  gameState.occupied++;
   var winner = false;
   for (var i = 0; i < 3; i++) { // Checks columns
-    if (board[0][i] === player && board[1][i] === player && board[2][i] === player) {
+    if (gameState.board[0][i] === player && gameState.board[1][i] === player && gameState.board[2][i] === player) {
       winner = true;
     }
   }
   for (var i = 0; i < 3; i++) { // Checks rows
-    if (board[i][0] === player && board[i][1] === player && board[i][2] === player) {
+    if (gameState.board[i][0] === player && gameState.board[i][1] === player && gameState.board[i][2] === player) {
       winner = true;
     }
   }
-  if (board[0][0] === player && board[1][1] === player && board[2][2] === player) { // Check major diagonal
+  if (gameState.board[0][0] === player && gameState.board[1][1] === player && gameState.board[2][2] === player) { // Check major diagonal
     winner = true;
   }
-  if (board[2][0] === player && board[1][1] === player && board[0][2] === player) { // Check minor diagonal
+  if (gameState.board[2][0] === player && gameState.board[1][1] === player && gameState.board[0][2] === player) { // Check minor diagonal
     winner = true;
   }
   if (winner) { // Check if they won
-    alert(`Player ${currentPlayer} won!`);
+    
+    if (gameState.currentPlayer === 1) {
+      gameState.p1Win++;
+      alert(`${gameState.p1Name} won!`);
+      gameState.lastWinner = 1;
+      document.getElementById("p1Win").innerHTML = `${gameState.p1Win}`;
+    } else {
+      gameState.p2Win++;
+      alert(`${gameState.p2Name} won!`);
+      gameState.lastWinner = 2;
+      document.getElementById("p2Win").innerHTML = `${gameState.p2Win}`;
+    }
     clearBoard();
-  } else if (occupied === 9) {
+  } else if (gameState.occupied === 9) {
     alert(`Tie detected! Board will reset`);
     clearBoard();
   } else {
-    if (currentPlayer === 1) {
-      currentPlayer++;
+    if (gameState.currentPlayer === 1) {
+      gameState.currentPlayer++;
     } else {
-      currentPlayer--;
+      gameState.currentPlayer--;
     }
   }
 }
 
-var cellClick = function() {
-  if (this.innerHTML === "") {
-    if(currentPlayer === 1) {
-      this.innerHTML = "X";
-      checkWin(this.id, currentPlayer);
-      document.getElementById("status").innerHTML = `Player ${currentPlayer}'s turn`;
-    } else if (currentPlayer === 2) {
-      this.innerHTML = "O";
-      checkWin(this.id, currentPlayer);
-      document.getElementById("status").innerHTML = `Player ${currentPlayer}'s turn`;
-    }
+var ticTacClick = function() {
+  if (this.id === "submitName1" || this.id === "submitName2") {
+    var nameProp = `p${this.id.charAt(this.id.length - 1)}Name`;
+    gameState[nameProp] = document.getElementById(nameProp).innerHTML;
+  } else if (this.id === "reset") {
+    clearBoard();
   } else {
-    document.getElementById("status").innerHTML = `<em>Nope, can't click there</em>`;
+    if (this.innerHTML === "") {
+      if(gameState.currentPlayer === 1) {
+        this.innerHTML = "X";
+        checkWin(this.id, gameState.currentPlayer);
+        document.getElementById("status").innerHTML = `${gameState.p1Name}'s turn`;
+      } else if (gameState.currentPlayer === 2) {
+        this.innerHTML = "O";
+        checkWin(this.id, gameState.currentPlayer);
+        document.getElementById("status").innerHTML = `${gameState.p2Name}'s turn`;
+      }
+    } else {
+      document.getElementById("status").innerHTML = `<em>Nope, can't click there</em>`;
+    }
   }
 }
